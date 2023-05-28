5b572a46c1829ae77b4e8053e23c0a58266725bf
Suhaib Mohamoud Hassan <suhaybmaxamuud55@gmail.com>
2023-05-28 19:36:19 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+  // Toggle mobile menu visibility
+  const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  mobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+
+//   Accordion 
+const mountain1 = document.getElementById('mountain1');
+mountain1.classList.add('bg-[#b0b4be]','text-[#414f6b]')
+const mountain2 = document.getElementById('mountain2');
+mountain2.classList.add('text-[#b0b4be]')
+const schedule = document.getElementById('schedule');
+function changeMountainToOne(element){
+    schedule.innerText = "Moutain 1"
+    if (mountain2.classList.contains('bg-[#b0b4be]','text-[#414f6b]')) {
+        mountain2.classList.remove('bg-[#b0b4be]','text-[#414f6b]');
+        mountain2.classList.add('text-[#b0b4be]');
+        mountain1.classList.remove('text-[#b0b4be]');
+         mountain1.classList.add('bg-[#b0b4be]','text-[#414f6b]');
+      } 
+      else{
+        console.log("Nothing to do");
+      }
+}
+function changeMountainToTwo(element){
+    schedule.innerText = "Moutain 2"
+
+    if (mountain1.classList.contains('bg-[#b0b4be]','text-[#414f6b]')) {
+        mountain1.classList.remove('bg-[#b0b4be]','text-[#414f6b]');
+        mountain1.classList.add('text-[#b0b4be]');
+        mountain2.classList.remove('text-[#b0b4be]');
+        
+
+        mountain2.classList.add('bg-[#b0b4be]','text-[#414f6b]');
+      } 
+}
